a2a717169fd62bd1aceac960dd9050442b4bef85
Kyle Alpert <kalpert@Kyles-MacBook-Pro.local>
2015-11-22 21:15:16 -0500

    create signup page

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+
 if __name__ == "__main__":
     app.run()
