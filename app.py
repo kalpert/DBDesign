2783df61e86ae4f593b40119999743452da54f8b
Kyle Alpert <kalpert94@gmail.com>
2015-11-22 21:43:10 -0500

    Reorganized files and added js jquery will be used for submissions. proper flask project structure

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import json
 
 app = Flask(__name__)
 
@@ -10,8 +12,22 @@ def main():
 
 
 @app.route('/showSignUp')
-def showSignUp():
+def show_sign_up():
     return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST'])
+def sign_up():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    if _name and _email and _password:
+        return json.dumps({'html': '<span>Word.</span>'})
+    else:
+        return json.dumps({'html': '<span>You messed up. Try again.</span>'})
+
+    # TODO: Actually validate and add user into database.
 
 
 if __name__ == "__main__":
