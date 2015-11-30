bfb225b12af38fd98978c6df7163b8d147b728a0
Kurt Marcinkiewicz <kurt.marcink@gmail.com>
2015-11-30 15:29:20 -0500

    use IIFE pattern - because JavaScript is good

--- static/js/sign-up.js
+++ static/js/sign-up.js
@@ -1,16 +1,18 @@
-$(function() {
+(function($) {
+  $(function() {
     $('#btnSignUp').click(function() {
 
-        $.ajax({
-            url: '/signUp',
-            data: $('form').serialize(),
-            type: 'POST',
-            success: function(response) {
-                console.log(response);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
+      $.ajax({
+               url: '/signUp',
+               data: $('form').serialize(),
+               type: 'POST',
+               success: function(response) {
+                 console.log(response);
+               },
+               error: function(error) {
+                 console.log(error);
+               }
+             });
     });
-});
+  });
+})(jQuery);
